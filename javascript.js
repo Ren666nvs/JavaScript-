01dd5987c0fac1e3fffd58c172f8c6f1e74a2698
Ren666nvs <lumbeb@Lumbes-MacBook-Air.local>
2024-12-02 12:12:53 +0800

    1

---
+++ javascript.js
@@ -0,0 +1,51 @@
+// console.log("hi");
+
+// let a = 5;
+//  let b = 1;
+//  let c = 2;
+// let p;
+//  p = a + b + c;
+// console.log(a + b + c);
+//  let d = 12;
+// let e = 23;
+// let p;
+// p = d + e;
+
+// console.log(p);
+// let f = 3;
+// console.log(f * f * f);
+// let h = 6;
+// let w = 4;
+// let p;
+// p = h * w;
+// console.log(p);
+// let i = 4;
+// let k = 5;
+// let v;
+// v = i * k;
+// console.log(v);
+
+// let q = -5;
+// let l = -3;
+// let d;
+// d = q - l;
+// console.log(d);
+// bodlogo-6
+let x2 = -2;
+let answer = 4* x2 *x2 -3* x2 +5;
+console.log(answer)
+// bodlogo-7
+let number3 =467;
+let suuliintoo = number3 % 10
+console.log(suuliintoo)
+// bodlogo-8
+let number4 = 467
+let aravt=(number4 % 100 ) /10
+console.log(aravt)
+// bodlogo-9
+let number5 =9
+let hoyrorntoitoo= (number5 3)git bran
+console.log(hoyrorntoitoo)
+let number6 =2
+let hoyroront=(number6 *7)
+console.log(hoyroront)
